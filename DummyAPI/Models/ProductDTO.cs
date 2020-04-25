d1e300e2e307d65ff17e836ecb56cbf1efb74ad3
Sebastian Chiappa <seba.chiappa.curbelo@gmail.com>
2020-04-25 17:45:39 -0300

    Se agrega ProductDTO y RememberMe en LoginDTO

---
+++ DummyAPI/Models/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DummyAPI.Models
+{
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
